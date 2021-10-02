0f22f35f671ef6612f630cc25bdce4c883b86284
Kjetil  Overby <kjeoever@msn.com>
2021-10-02 08:00:15 +0200

    test

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useAuth0 } from "@auth0/auth0-react";
-
+//test
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
